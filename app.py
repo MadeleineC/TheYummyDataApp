2436ed6eb3b4562ef36707fd71ba74ed0ee0bba8
Yizhi Yin <yizhiyin86@gmail.com>
2018-05-30 14:55:03 -0500

    database hooked up

--- app.py
+++ app.py
@@ -48,34 +48,6 @@ user_input=Base.classes.user_input
 zip_shapes=Base.classes.zip_shapes
 
 
-
-###
-# Path Layout:
-#   /fetch + form data 
-#               -- put form data into object    
-#           -- run API call with form data
-#               <- return API call as object 
-#       -> /zips/<citystate>
-#           -- search ziplist by citystate ziplist
-#               <- return demographics info
-#           OR
-#           -- search python zipcode package by citystate
-#               <- return demographics info
-#   -- plot with info
-###
-
-###
-# Step 2:
-#   /fetch + form data
-#           -- put form data into object
-#       -- run API call with form data
-#           <- return API call as object
-#       -> /zips/<citystate>
-#           -- query zipsDB by citystate
-#               <- return demographic info
-#   -- plot with info
-###
-
 ###
 # ideal:
 #   /fetch + form data
@@ -96,6 +68,70 @@ zip_shapes=Base.classes.zip_shapes
 #################################################
 # Functions
 #################################################
+def check_state(key):
+    key = key.lower()
+    key = ' '.join(word[0].upper() + word[1:] for word in key.split())
+    us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY',
+        'Washington, DC': 'DC',
+        'District of Columbia': 'DC'
+    }
+    
+    if key in us_state_abbrev.keys():
+        return str(us_state_abbrev[key]).lower()
+    elif key.upper() in us_state_abbrev.values():
+        return key.lower()
+    else:
+        return "Not found"
 
 def get_demo(**kwargs):
     print("in get_demo")
@@ -119,22 +155,28 @@ def get_demo(**kwargs):
         feature={}
         zip_search = i.Zipcode
         population=i.Population
-        income_per_capita=i.Wealthy
+        income=i.Wealthy
+
+        #query geocoordinates from the zip_shape table in the DB
         geometry=session.query(zip_shapes.geometry).filter(zip_shapes.zip_code==int(zip_search))[0][0]
+        
+        #pass the string of geocoordinates json.load
         start_index=geometry.find('[[')
-        # coordinate=geometry[start_index:-1]
         coordinate=json.loads(geometry[start_index:-1])
         type_end=geometry.find("', 'coordinates")
         geo_type=geometry[10:type_end]
-#     coordinates=json.loads(geometry[35:-1])
+
+        #pull in information into one geoJSON feature 
         feature={'type':'Feature',\
-             'properties':{'zipcode':zip_search,'population':population,'income_per_capita':i.Wealthy},\
+             'properties':{'zipcode':zip_search,'population':population,'income_per_capita':income},\
             'geometry':{'type':geo_type,'coordinates':coordinate}\
                 }
+        #remove any postbox zipcode with a population with 0
         if feature['properties']['population']==0:
             print('>>>>>>>>>>>>>>>>>> delete' + feature['properties']['zipcode'])
         else:
             features.append(feature)
+    #put all the features in to a feature_collections object
     feature_collections={"type": "FeatureCollection","crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" } },"name": "cb_2017_us_zcta510_500k","features":features}
     return feature_collections
 
@@ -149,15 +191,7 @@ def getYummyInfo(**kwargs):
         city = "waco"
         state= "tx"
         cuisine = "sushi"
-    # print(city)
-    # pythondata=[{'foo':'bar','baz':'jazz'}]
-    # pythondata=[
-    #             {
-    #                 'city':'austin',
-    #                 'state':'tx',
-    #                 'cuisine':'french'
-    #                 }
-    #             ]
+    
     yummy_info=[
                 {
                     'city':city,
@@ -166,9 +200,7 @@ def getYummyInfo(**kwargs):
                     }
                 ]
     print(yummy_info)
-    # print(json.dumps(yummy_info, default=lambda o: o.__dict__))
-    # yummy_info = json.dumps(yummy_info)
-    # yummy_info = json.loads(yummy_info)
+    
     return yummy_info
 
 def get_ziplist(**kwargs):
@@ -231,7 +263,7 @@ def apiCall(**kwargs):
         next_page=response.get("next_page_token")
         
         #sometimes it give me empty results if I make calls too often 
-        sleep(1)
+        sleep(0.5)
         
         #use a while loop for flipping pages, google allows a maximum of 3 pages 
         while bool(next_page)==True: 
@@ -262,9 +294,10 @@ def apiCall(**kwargs):
 
 def clean_google_results(**kwargs):
     print("in clean_google_results")
-    print(kwargs)
-    if("google_results" in kwargs): 
+    # print(kwargs)
+    if("google_results" in kwargs and "city_state_cuisine" in kwargs): 
         google_results = kwargs['google_results']
+        city_state_cuisine=kwargs['city_state_cuisine']
         
     else:
         google_results = []
@@ -274,11 +307,14 @@ def clean_google_results(**kwargs):
         if ('formatted_address' in google_results[i]):
             address = google_results[i]['formatted_address']
             zip_store = re.findall('[A-Z]{2} [0-9]{5}',google_results[i]['formatted_address'])
-            zip_store = zip_store[0][3:]
-            print(zip_store)
+            if zip_store==[]:
+                zip_store='Nan'
+            else:
+                zip_store = zip_store[0][3:]
+            # print(zip_store)
         else:
             address = 'Nan'
-        google_id = google_results[i]['id']
+
         latitude =  google_results[i]['geometry']['location']['lat']
         longitude = google_results[i]['geometry']['location']['lng']
         business_name = google_results[i]['name']
@@ -292,28 +328,29 @@ def clean_google_results(**kwargs):
             rating = 0
         item = {
             'Address' : address,
-            'Google_ID' : google_id,
             'Latitude' : latitude,
             'Longitude' : longitude,
             'Name' : business_name,
             'Price_Level' : price,
             'Rating' : rating,
-            'Zip' : zip_store
-        }
+            'Zip' : zip_store,
+            'City_State_Cuisine':city_state_cuisine
+            }
         #check if the restaurant is in the cleaned_google_results list already
         Flag = 'Not Found'
         for stored_item in cleaned_google_results:
-            if item['Google_ID']==stored_item['Google_ID']:
+            if item['Address']==stored_item['Address']:
                 Flag='Found'
                 break
-        if address == 'Nan' or Flag=='Found':
+        if address == 'Nan' or Flag=='Found' or zip_store=='Nan':
             print('************> deleted' + business_name )
         else:
             cleaned_google_results.append(item)
-            print(i)
-            print(item)
+            # print(i)
+            # print(item)
     #remove duplicate restaurants and convert it back to list
     
+
     
     print('done')
     # print(cleaned_google_results[0])
@@ -331,128 +368,62 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-# @app.route("/")
-# def home():
-#     """Render Home Page."""
-#     return render_template("dashboard.html")
-
-#get route to return zip results
-@app.route("/zips/<city_state>")
-def zips(city_state):
-    """Return sample names"""
-
-    # sample names are column (from the 2nd column) names for the table samples
-    results = session.query(zip_demo).filter(zip_demo.City_State==city_state).all()
-    #convert result into dictionary
-    results_list=[]
-    for i in results:
-        dict_result=i.__dict__
-        del dict_result['_sa_instance_state']
-        results_list.append(dict_result)
-    return jsonify(results_list)
-
 #get route for restaurant results
-@app.route("/restaurant/") #need to add <city_state_type>
-def restarant():
-    """Return sample names"""
-
-    # sample names are column (from the 2nd column) names for the table samples
-    results = session.query(restaurant).all()
-    #convert result into dictionary
-    results_list=[]
-    for i in results:
-        dict_result=i.__dict__
-        del dict_result['_sa_instance_state']
-        results_list.append(dict_result)
-    return jsonify(results_list)
-
-#### original:
-# #get route for restaurant results
-# @app.route("/fetch/", methods=["GET", "POST"]) #need to add <city_state_type>
-# def fetch():
-#     """send variables to python"""
-#     if request.method == "POST":
-#         city = request.form["city"]
-#         city = city.lower()
-#         state = request.form["state"]
-#         state = state.lower()
-#         cuisine = request.form["cuisine"]
-#         cuisine = cuisine.lower()
-
-
-#         demographics = get_demo(city, state)
-#         print(demographics)
-
-#         # api_results = api_call(city, state, cuisine)
-#         # print(api_results)
-
-#         # pet = Pet(name=name, lat=lat, lon=lon)
-#         # db.session.add(pet)
-#         # db.session.commit()
-#         print(city)
-#         print(state)
-#         print(cuisine)
-#         city_state = city+state
-#         print(city_state)
-#         redirect_link = "/zips/"+city_state
-#         print(redirect_link)
-#         # return jsonify(a[0].City)
-#         # return redirect("http://localhost:5000/", code=302)
-#         return redirect(redirect_link)
-#     return render_template("index.html")
-
-
-#get route for restaurant results
-@app.route("/fetch/", methods=["GET", "POST"]) #need to add <city_state_type>
+@app.route("/fetch/", methods=["GET", "POST"]) 
 def fetch():
     """send variables to python"""
     if request.method == "POST":
         city = request.form["city"]
-        city = city.lower()
+        city = city.lower().strip(' ')
         state = request.form["state"]
-        state = state.lower()
+        state = state.strip(' ')
+        state = check_state(state)
         cuisine = request.form["cuisine"]
-        cuisine = cuisine.lower()
+        cuisine = cuisine.lower().strip(' ')
+        
+        # if a wrong state is in input set to the default
+        if state=="Not found":
+            demographics = get_demo()
+            yummy_info=getYummyInfo()
 
-        print("----")
-        demographics = get_demo(city=city, state=state)
-        # print(demographics)
-        # demographics = json.loads(demographics)
-        print("ZZZZZZ")
-        print(demographics)
+        else:
+            demographics=get_demo(city=city,state=state)
+            yummy_info=getYummyInfo(city=city,state=state,cuisine=cuisine)
 
-        # api_results = api_call(city, state, cuisine)
-        # print(api_results)
+            #query if city_state_cuisine is in the database already
+            city_state_cuisine='-'.join([city,state,cuisine])
+            print('\n*******************\n Back in fetch city_state_cuisine is {}'.format(city_state_cuisine))
+            search=session.query(user_input).filter(user_input.City_State_Cuisine==city_state_cuisine).first()
+            if search == None:
+                print('******************** search back in fetch')
+                google_results = apiCall(city=city, state=state, cuisine=cuisine)
+                print("********")
+                # print(google_results)
+                print("TTTTTT")
+                # print(google_results[0])
+                google_results = clean_google_results(google_results=google_results,city_state_cuisine=city_state_cuisine)
+                
+                #insert into the database if the google_results are not empty
+                if google_results !=[]:
+                    conn = engine.connect()
+                    conn.execute(restaurant.__table__.insert(), google_results)
+                    new_user=[{'City':city,'State':state,'Cuisine':cuisine,
+                            'City_State':'-'.join([city,state]),'City_State_Cuisine':city_state_cuisine}]
+                    conn.execute(user_input.__table__.insert(),new_user)
+                print("SSSSS")
+                print(google_results)
+            else:
+                print('DDDDDDDDDDDDDD DATABASE')
+                results=session.query(restaurant).filter(restaurant.City_State_Cuisine==city_state_cuisine).all()
+                google_results=[]
+                for i in results:
+                    dict_result=i.__dict__
+                    del dict_result['_sa_instance_state']
+                    google_results.append(dict_result)
+                print('SSSSSSSSSSS-Queried-Result')
+                print(google_results[0])
 
-        # pet = Pet(name=name, lat=lat, lon=lon)
-        # db.session.add(pet)
-        # # db.session.commit()
-        # print(city)
-        # print(state)
-        # print(cuisine)
-
-        yummy_info = getYummyInfo(city=city, state=state, cuisine=cuisine)
-        # yummy_info = json.loads(yummy_info)
-        print('b')
-        # print(yummy_info)
-
-        google_results = apiCall(city=city, state=state, cuisine=cuisine)
-        print("XXXX")
-        # print(google_results)
-        print("TTTTTT")
-        # print(google_results[0])
-        google_results = clean_google_results(google_results=google_results, state=state)
-        print("SSSSS")
-        print(google_results)
-
-        # city_state = city+state
-        # print(city_state)
-        # redirect_link = "/zips/"+city_state
-        # print(redirect_link)
-        # return jsonify(a[0].City)
-        # return redirect("http://localhost:5000/", code=302)
-        # return redirect(redirect_link)
-    return render_template("dashboard.html", demographics=demographics, yummy_info=yummy_info, google_results=google_results)
+    return render_template("dashboard.html", demographics=demographics, yummy_info=yummy_info , google_results=google_results)
 
 @app.route('/getpythondata/')
 def get_python_data():
@@ -471,11 +442,42 @@ def get_python_data():
 @app.route('/')
 @app.route('/dashboard/')
 def dashboard():
-    print("initial page load")
+    print("\nGGGGGGGGGGGGGGGGGG\n At DASHBOARD")
     demographics = get_demo()
     yummy_info = getYummyInfo()
-    google_results = apiCall()
-    google_results = clean_google_results(google_results=google_results) #,state='TX')
+    city=yummy_info[0]['city']
+    state=yummy_info[0]['state']
+    cuisine=yummy_info[0]['cuisine']
+    city_state_cuisine='-'.join([city,state,cuisine])
+    print("GGGGGGGGGGGGGGGG---city_state_cuisine{}".format(city_state_cuisine))
+    search=session.query(user_input).filter(user_input.City_State_Cuisine==city_state_cuisine).first()
+    if search == None:
+        google_results = apiCall(city=city, state=state, cuisine=cuisine)
+        print("XXXX")
+        # print(google_results)
+        print("TTTTTT")
+        # print(google_results[0])
+        google_results = clean_google_results(google_results=google_results,city_state_cuisine=city_state_cuisine)
+        #insert into the database if the google_results are not empty
+        if google_results !=[]:
+            conn = engine.connect()
+            conn.execute(restaurant.__table__.insert(), google_results)
+            new_user=[{'City':city,'State':state,'Cuisine':cuisine,
+                            'City_State':'-'.join([city,state]),'City_State_Cuisine':city_state_cuisine}]
+            conn.execute(user_input.__table__.insert(),new_user)
+        print("SSSSS")
+        print('Inserted into DATABASE')
+    else:
+        print('DDDDDDDDDDDDDD QUERY DATABASE')
+        results=session.query(restaurant).filter(restaurant.City_State_Cuisine==city_state_cuisine).all()
+        google_results=[]
+        for i in results:
+            dict_result=i.__dict__
+            del dict_result['_sa_instance_state']
+            google_results.append(dict_result)
+        print('SSSSSSSSSSS-Queried-Result')
+        print(google_results[0])
+
     return render_template("dashboard.html", demographics=demographics, yummy_info=yummy_info, google_results=google_results)
 
 #deal with crossorgin issue
